6d1edc488dc99dd393e1cabffb1390cea3574167
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-12-19 15:25:06 +0100

    rutas modificadas de usuario

--- src/app/elements/services/usuario.service.ts
+++ src/app/elements/services/usuario.service.ts
@@ -11,22 +11,22 @@ export class UsuarioService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>('https://proyecto-bbdd-production-faf3.up.railway.app/api/usuarios');
+    return this.http.get<Usuario[]>('https://proyecto-bbdd-production-faf3.up.railway.app/usuarios');
   }
 
   get(id: any) {
-    return this.http.get('https://proyecto-bbdd-production-faf3.up.railway.app/api/usuario/'+id);
+    return this.http.get('https://proyecto-bbdd-production-faf3.up.railway.app/usuario/'+id);
   }
 
   post(data: any){
-    return this.http.post('https://proyecto-bbdd-production-faf3.up.railway.app/api/usuario',data);
+    return this.http.post('https://proyecto-bbdd-production-faf3.up.railway.app/register',data);
   }
 
   put(id:any, data: any){
-    return this.http.put(`https://proyecto-bbdd-production-faf3.up.railway.app/api/usuario/${id}`, data);
+    return this.http.put(`https://proyecto-bbdd-production-faf3.up.railway.app/usuario/${id}`, data);
   }
 
   delete(id: any) {
-    return this.http.delete('https://proyecto-bbdd-production-faf3.up.railway.app/api/usuario/'+id);
+    return this.http.delete('https://proyecto-bbdd-production-faf3.up.railway.app/usuario/'+id);
   }
 }
